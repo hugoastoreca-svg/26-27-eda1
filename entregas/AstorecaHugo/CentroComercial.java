c7bf732edcb1d899cc86a8cec6f31d8feed2482e
hugoastoreca <hugo.astoreca@alumnos.uneatlantico.es>
2026-09-19 14:12:46 +0200

    feat: se crea toda la estructura del programa en la clase centro Comercial.

--- entregas/AstorecaHugo/CentroComercial.java
+++ entregas/AstorecaHugo/CentroComercial.java
@@ -1,7 +1,56 @@
+
 public class CentroComercial {
 
-    void simular() {
-        
-    
-}
+    private final double PROBABILIDAD_LLEGADA_CLIENTE = 0.6;
+    private final double PROBABILIDAD_DE_CAJA_VACIA = 0.4;
+    private final int NUMERO_CAJAS = 4;
+    private Fila filas;
+    private Tiempo tiempo;
+    private Caja[] cajas;
+    private boolean haLlegadoCliente;
+    private Console console;
+
+    public CentroComercial() {
+        console = new Console();
+        filas = new Fila();
+        tiempo = new Tiempo();
+        cajas = new Caja[NUMERO_CAJAS];
+        for (int i = 0; i < NUMERO_CAJAS; i++) {
+            cajas[i] = new Caja();
+        }
+    }
+
+    public void simular() {
+        do {
+
+            tiempo.avanzarTiempo();
+            this.procesarLlegadaCliente();
+            this.registrarEstado();
+            this.asignarClienteACaja();
+            this.procesarCajas();
+            this.mostrarEstado();
+
+        } while (!tiempo.haFinalizado());
+
+    }
+
+    private void procesarLlegadaCliente() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void registrarEstado() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void asignarClienteACaja() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void procesarCajas() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void mostrarEstado() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
