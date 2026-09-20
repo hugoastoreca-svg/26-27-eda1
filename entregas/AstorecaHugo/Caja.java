de18078b93544bdba727f5c7c9430dbfc35c4a29
hugoastoreca <hugo.astoreca@alumnos.uneatlantico.es>
2026-09-20 15:19:54 +0200

    feat: cojo la clase caja de el carrefour que ya estaba hecho

--- entregas/AstorecaHugo/Caja.java
+++ entregas/AstorecaHugo/Caja.java
@@ -1,3 +1,53 @@
 public class Caja {
-    
+      protected int numero;
+    private Cliente cliente;
+    private int itemsVendidos;
+    private int personasAtendidas;
+    protected int itemsRestantes;
+    private Console console;
+
+    public Caja(int numero) {
+        this.numero = numero;
+        itemsVendidos = 0;
+        personasAtendidas = 0;
+        itemsRestantes = 0;
+        console = new Console();
+    }
+
+    public boolean estaLibre() {
+        return cliente == null;
+    }
+
+    public void asignar(Cliente cliente) {
+        this.cliente = cliente;
+        itemsRestantes = cliente.obtenerItems();
+    }
+
+    public void avanzarAtencion() {
+        if (!this.estaLibre()) {
+            itemsRestantes = itemsRestantes - 1;
+            if (itemsRestantes == 0) {
+                personasAtendidas = personasAtendidas + 1;
+                itemsVendidos = itemsVendidos + cliente.obtenerItems();
+                cliente = null;
+            }
+        }
+    }
+
+    public void mostrar() {
+        console.write("Caja ["+numero+"] ");
+        console.writeln("[:]".repeat(itemsRestantes));
+    }
+
+    public int obtenerPersonasAtendidas() {
+        return personasAtendidas;
+    }
+
+    public int obtenerItemsVendidos() {
+        return itemsVendidos;
+    }
+
+    public boolean puedeAtender(Cliente cliente){
+        return true;
+    }
 }
