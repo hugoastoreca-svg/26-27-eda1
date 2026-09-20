03a54559302632a4d7755abf5889c62d5d75251c
hugoastoreca <hugo.astoreca@alumnos.uneatlantico.es>
2026-09-20 15:49:24 +0200

    feat:creo la clase tiempo

---
+++ entregas/AstorecaHugo/Fila.java
@@ -0,0 +1,60 @@
+public class Fila {
+
+     private Cliente[] clientes;
+    private final int CAPACIDAD_MAXIMA = 100;
+    private int minutosSinClientes;
+    private int tamaño;
+    private Console console;
+
+    public Fila() {
+        clientes = new Cliente[CAPACIDAD_MAXIMA];
+        minutosSinClientes = 0;
+        tamaño = 0;
+        console = new Console();
+    }
+
+    public void registrarEstado() {
+        if (tamaño == 0) {
+            minutosSinClientes = minutosSinClientes + 1;
+        }
+    }
+
+    public void añadirCliente(Cliente cliente) {
+        clientes[tamaño] = cliente;
+        tamaño = tamaño + 1;
+    }
+
+    public boolean hayClientes() {
+        return tamaño > 0;
+    }
+
+    public Cliente quitarCliente() {
+        Cliente cliente = clientes[0];
+        for (int i = 0; i < tamaño - 1; i++) {
+            clientes[i] = clientes[i + 1];
+        }
+        clientes[tamaño - 1] = null;
+        tamaño = tamaño - 1;
+        return cliente;
+    }
+
+    public void mostrar() {
+        for(int i=0;i<tamaño;i++){
+            clientes[i].mostrar();
+        }
+        console.writeln();
+    }
+
+    public int obtenerMinutosSinClientes() {
+        return minutosSinClientes;
+    }
+
+    public int obtenerCantidadPersonasEnCola() {
+        return tamaño;
+    }
+
+    public Cliente primero() {
+        return clientes[0];
+    }
+    
+}
