7038017f8f4d0fada04b97f1e2680046ed7c1a4c
hugoastoreca <hugo.astoreca@alumnos.uneatlantico.es>
2026-09-20 15:45:15 +0200

    feat: hago la clase cliente

---
+++ entregas/AstorecaHugo/Cliente.java
@@ -0,0 +1,25 @@
+public class Cliente {
+    private int items;
+    private Console console;
+
+    public Cliente() {
+        items = this.generarItems();
+        console = new Console();
+    }
+
+    private int generarItems() {
+        final int MAXIMO_ITEMS = 15;
+        final int MINIMO_ITEMS = 5;
+        return (int) (Math.random() * (MAXIMO_ITEMS - MINIMO_ITEMS) + MINIMO_ITEMS);
+    }
+
+    public int obtenerItems() {
+        return items;
+    }
+
+    public void mostrar() {
+        console.write("[" + items + "]_O/");
+    }
+
+    
+}
